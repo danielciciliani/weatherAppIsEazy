3bf0b7b15b66252498526ce795d67bb8404f89c9
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-06-04 00:30:15 +0200

    feat(api): add weather api fetch to services

---
+++ src/services/weatherApiCall.ts
@@ -0,0 +1,22 @@
+import { cities } from "../data/cities";
+
+const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
+
+export async function getWeatherData(lat: number, long: number, city='London'){
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`;
+  
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+
+
