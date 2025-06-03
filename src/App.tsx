cfd464728415e093c3067c831201152a6cddc3d7
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-06-03 23:19:56 +0200

    chore: fist commit, initial setup and tailwind

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+
+
+function App() {
+
+  return (
+    <>
+    <div className=''>
+      <h1 className='capitalize'>weather app</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
