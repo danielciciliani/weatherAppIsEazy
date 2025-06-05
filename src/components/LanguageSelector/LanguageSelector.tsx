80cf22e6404d0474caa873a8199befe44fe6aa43
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-06-05 11:51:03 +0200

    feat: add languageSelector component

---
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const availableLanguages = ['en', 'es'];
+
+
+function LanguageSelector({selectedLanguage, setSelectedLanguage}) {
+
+  return (
+    <>
+    <div className="flex row gap-5 pt-3">
+      { availableLanguages.map((language) => (
+        <button key={language} onClick={() => setSelectedLanguage(language)} 
+        className={`uppercase cursor-pointer hover:text-white transition-all ease duration-200
+         ${selectedLanguage === language ? 'text-white underline' : ''} `}
+         >{language}</button>
+    ))}
+    </div>
+    </>
+  );
+}
+
+export default LanguageSelector;
+
