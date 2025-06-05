28820d32bf2d745eab961d797c4a41a22e2fed58
danielciciliani <55026851+danielciciliani@users.noreply.github.com>
2025-06-05 11:49:53 +0200

    fix: remove unused parameter in api call

--- src/services/weatherApiCall.ts
+++ src/services/weatherApiCall.ts
@@ -1,7 +1,7 @@
 const API_KEY = import.meta.env.VITE_WEATHER_API_KEY;
 
-export async function getWeatherData(lat: number, long: number, city='London'){
-  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}`;
+export async function getWeatherData(lat: number, long: number, lang: string){
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${API_KEY}&lang=${lang}`;
   
   try {
     const response = await fetch(URL);
